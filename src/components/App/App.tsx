6a9e8b444418481756eece2335b2a17df8c1e5a0
ValberMedeiros <dev.junior.medeiros@gmail.com>
2020-09-19 01:24:38 -0300

    create TestComponent and move App to the components directory

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestComponent from '../TestComponent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ola Mundo!
-        </p>
+        <TestComponent></TestComponent>
         <a
           className="App-link"
           href="https://reactjs.org"
