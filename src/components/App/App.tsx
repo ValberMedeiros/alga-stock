5a49e3780245c566a59c4afcac9a5a664e7ff3b1
ValberMedeiros <dev.junior.medeiros@gmail.com>
2020-09-19 12:43:35 -0300

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestComponent from '../TestComponent'
-import ClassComponent from '../ClassComponent'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent name="Junior"></TestComponent>
-        <ClassComponent name="Mundo"></ClassComponent>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      APP
     </div>
   );
 }
