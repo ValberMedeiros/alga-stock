b32f333ffc8d93da86ebbc63eee37a8ac440fd65
ValberMedeiros <dev.junior.medeiros@gmail.com>
2020-09-21 19:43:40 -0300

    clear app + add table componet's base files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Button from '../shared/Button';
 import Header from '../Header';
 import Container from '../shared/Container';
-import Input from '../shared/Input';
 
 function App() {
-  const [street, setStreet] = useState('');
 
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <Button content="Click me">
-        </Button>
-        <Input
-          label="Street"
-          placeholder="E.g.: 15th Avenue"
-          value={ street }
-          onChange={e => setStreet(e.target.value)}
-        />
-      </Container>
 
+      </Container>
     </div>
   );
 }
