95068d727a0db6ee61ae9b3d7ee21aa2e4019e9c
ValberMedeiros <dev.junior.medeiros@gmail.com>
2020-09-24 20:16:10 -0300

    move product crud logic to the ProductsCRUD component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,107 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from '../Header';
 import Container from '../shared/Container';
-import Table, { TableHeader } from '../shared/Table';
-import { Product } from '../shared/Table/Table.mockdata';
-import ProductForm, { ProductCreator } from '../Products/ProductForm';
-import Swal from 'sweetalert2'
-import { getAllProducts, createSingleProduct, updateSingleProduct, deleteSingleProduct } from '../../services/Product.service';
-
-const headers: TableHeader[] = [
-  { key: 'id', value: '#' },
-  { key: 'name', value: 'Product' },
-  { key: 'price', value: 'Price', right: true },
-  { key: 'stock', value: 'Available Stock', right: true }
-]
+import ProductsCRUD from '../Products/ProductsCRUD';
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [updateProducts, setUpdateProducts] = useState<Product | undefined>(products[0])
-  
-  async function fetchData() {
-    const _products = await getAllProducts()
-    setProducts(_products)
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  const handleProductSubmit = async (product: ProductCreator) => {
-    try {
-      await createSingleProduct(product)
-      fetchData()
-    } catch (error) {
-      Swal.fire('Oops!', error.message, 'error')
-    }
-  }
-
-  const handleProductUpdate = async (newProduct: Product) => {
-    try {
-      await updateSingleProduct(newProduct)
-      setUpdateProducts(undefined)
-      fetchData()
-    } catch (error) {
-      Swal.fire('Oops!', error.message, 'error')
-    }
-  }
-
-  const handleProductEdit = (product: Product) => {
-    setUpdateProducts(product)
-  }
-
-  const handleProductDetail = (product: Product) => {
-    Swal.fire(
-      'Product details',
-      `${product.name} costs $${product.price} and we have ${product.stock} available in stock.`,
-      'info'
-    )
-  }
-
-  const deleteProduct = async (_id: string) => {
-    try {
-      await deleteSingleProduct(_id)
-      fetchData()
-      Swal.fire('Uhul!', 'Product successgully deleted', 'success')
-    } catch (error) {
-      Swal.fire('Oops!', error.message, 'error')
-    }
-  }
-
-  const handleProductDelete = (product: Product) => {
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#09f',
-      cancelButtonColor: '#d33',
-      confirmButtonText: `Yes, delete ${product.name}!`
-    }).then((result) => {
-      if (result.isConfirmed) {
-        deleteProduct(product._id);
-      }
-    })
-  }
-
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <Table 
-          headers={ headers }
-          data={ products }
-          enableActions
-          onDelete={ handleProductDelete }
-          onDetail={ handleProductDetail }
-          onEdit={ handleProductEdit }
-        />
-        <ProductForm 
-          form={ updateProducts }
-          onSubmit={ handleProductSubmit } 
-          onUpdate={ handleProductUpdate }
-        />
+        <ProductsCRUD />
       </Container>
     </div>
   );
---
+++ src/components/Products/ProductsCRUD.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react'
+import Swal from 'sweetalert2';
+import { createSingleProduct, deleteSingleProduct, getAllProducts, updateSingleProduct } from '../../services/Product.service';
+import Table, { TableHeader } from '../shared/Table'
+import { Product } from '../shared/Table/Table.mockdata';
+import ProductForm, { ProductCreator } from './ProductForm';
+
+
+const headers: TableHeader[] = [
+  { key: 'id', value: '#' },
+  { key: 'name', value: 'Product' },
+  { key: 'price', value: 'Price', right: true },
+  { key: 'stock', value: 'Available Stock', right: true }
+]
+
+const ProductsCRUD = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [updateProducts, setUpdateProducts] = useState<Product | undefined>(products[0])
+  
+  async function fetchData() {
+    const _products = await getAllProducts()
+    setProducts(_products)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const handleProductSubmit = async (product: ProductCreator) => {
+    try {
+      await createSingleProduct(product)
+      fetchData()
+    } catch (error) {
+      Swal.fire('Oops!', error.message, 'error')
+    }
+  }
+
+  const handleProductUpdate = async (newProduct: Product) => {
+    try {
+      await updateSingleProduct(newProduct)
+      setUpdateProducts(undefined)
+      fetchData()
+    } catch (error) {
+      Swal.fire('Oops!', error.message, 'error')
+    }
+  }
+
+  const handleProductEdit = (product: Product) => {
+    setUpdateProducts(product)
+  }
+
+  const handleProductDetail = (product: Product) => {
+    Swal.fire(
+      'Product details',
+      `${product.name} costs $${product.price} and we have ${product.stock} available in stock.`,
+      'info'
+    )
+  }
+
+  const deleteProduct = async (_id: string) => {
+    try {
+      await deleteSingleProduct(_id)
+      fetchData()
+      Swal.fire('Uhul!', 'Product successgully deleted', 'success')
+    } catch (error) {
+      Swal.fire('Oops!', error.message, 'error')
+    }
+  }
+
+  const handleProductDelete = (product: Product) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#09f',
+      cancelButtonColor: '#d33',
+      confirmButtonText: `Yes, delete ${product.name}!`
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteProduct(product._id);
+      }
+    })
+  }
+
+  return <>
+    <Table 
+      headers={ headers }
+      data={ products }
+      enableActions
+      onDelete={ handleProductDelete }
+      onDetail={ handleProductDetail }
+      onEdit={ handleProductEdit }
+    />
+    <ProductForm 
+      form={ updateProducts }
+      onSubmit={ handleProductSubmit } 
+      onUpdate={ handleProductUpdate }
+    />
+  </>
+}
+
+export default ProductsCRUD;
