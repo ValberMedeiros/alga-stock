77d12059b16e41409532a2cabae0439486cef74b
ValberMedeiros <dev.junior.medeiros@gmail.com>
2020-09-24 19:52:32 -0300

    add redux devtools

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -5,6 +5,10 @@ const reducers = combineReducers({
   products: Products
 })
 
-const store = createStore(reducers)
+const store = createStore(
+  reducers,
+  //@ts-ignore
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store;
