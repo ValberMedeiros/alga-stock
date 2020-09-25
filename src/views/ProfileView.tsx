ce916f0010675518d1bbd314adffa69533a1ee7a
ValberMedeiros <dev.junior.medeiros@gmail.com>
2020-09-25 00:09:36 -0300

    create profile view + card

---
+++ src/views/ProfileView.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import ProfileCard, { User } from '../components/Authentication/ProfileCard';
+import Header from '../components/Header';
+import Container from '../components/shared/Container';
+
+declare interface ProfileViewProps {
+  user: User
+}
+
+const ProfileView:React.FC<ProfileViewProps> = (props) => {
+  return <>
+  <Header title="AlgaStock" />
+  <Container>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center' 
+    }}>
+      <ProfileCard user={ props.user }/>
+    </div>
+  </Container>
+  </>
+}
+
+const mapStateToProps = () => ({
+  user: {
+    name: 'Junior Medeiros',
+    email: 'junior_odm@hotmail.com'
+  }
+})
+
+export default connect(mapStateToProps) (ProfileView)
